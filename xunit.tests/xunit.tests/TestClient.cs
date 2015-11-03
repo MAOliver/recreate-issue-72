ffda58b1afc80a6a96aea055db9ff0785a3aca05
moliver <matthewaustinoliver@gmail.com>
2015-11-03 00:44:54 -0500

    added basic test to recreate problem

---
+++ xunit.tests/xunit.tests/TestClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Models;
+using Xunit;
+
+namespace xunit.tests
+{
+    public class TestClient
+    {
+        [Fact(DisplayName = "Should map client to EF Client")]
+        public void TestAddClient()
+        {
+
+            var entity = new Client()
+            {
+                ClientId = "test",
+                ClientName = "test2",
+                AllowedScopes = new List<string> { "item1", "item2" },
+                RedirectUris = new List<string> { "http://redirect1" },
+                AccessTokenType = AccessTokenType.Jwt,
+                Flow = Flows.ResourceOwner,
+                ClientSecrets = new List<Secret> { new Secret("secret") }
+
+
+            }.ToEntity();
+
+            Assert.NotNull(entity);
+        }
+    }
+}
